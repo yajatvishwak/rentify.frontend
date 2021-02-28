964c8112b3cabe1c97f9ec74554e9f3f4a326633
yajatvishwak <yajat472001@gmail.com>
2021-02-28 10:54:35 +0530

    first commit

--- src/routes/ChatRoom.svelte
+++ src/routes/ChatRoom.svelte
@@ -1,20 +1,41 @@
 <script>
   import Tobar from "./components/Tobar.svelte";
   import Axios from "axios";
-  let part;
-  Axios.post("http://localhost:5000/user/getchatroom").then((res) => {
+  import { store } from "../store";
+  let username = "";
+  store.subscribe((val) => {
+    username = JSON.parse(val).username;
+  });
+
+  import { push } from "svelte-spa-router";
+  let part = [{}];
+  Axios.post("http://localhost:3000/user/getChatRoom", {
+    username: username,
+  }).then((res) => {
     part = res.data;
+    console.log(res.data);
+    part = res.data.map((item) => {
+      const participant1 = item.participants[0];
+      const participant2 = item.participants[1];
+      const actualParticipant =
+        participant1 === username ? participant2 : participant1;
+      return { actualPart: actualParticipant, roomID: item.roomID };
+    });
+    console.log(part);
   });
 </script>
 
 <div class="container">
   <Tobar />
   <div class="title">Chat with other users</div>
+
   <div class="listContainer">
-    <div class="chatGroup">
-      <div class="title">Ayush</div>
-      <img class="profileimg" src="https://via.placeholder.com/120" />
-    </div>
+    {#each part as p}
+      <div on:click={() => push("/chatroom/" + p.roomID)} class="chatGroup">
+        <div class="title">{p.actualPart}</div>
+        <img class="profileimg" src="https://via.placeholder.com/120" />
+      </div>
+    {/each}
   </div>
 </div>
 
